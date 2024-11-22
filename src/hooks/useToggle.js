58a79a2e1f5f032913532829c2b91405438e2897
Konstantin1spb <105925642+sitepreview@users.noreply.github.com>
2024-11-22 18:48:36 +0300

    feat: useToggle

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,1 +1,37 @@
+import { useReducer } from 'react';
 
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'SET_VALUE':
+			return action.payload;
+		default:
+			return state;
+	}
+};
+
+export const useToggle = (arr) => {
+	const [value, dispatch] = useReducer(reducer, arr[0]);
+
+	const setExactValue = (val) => {
+		return dispatch({
+			type: 'SET_VALUE',
+			payload: val,
+		});
+	};
+
+	const toggle = (val) => {
+		if (arr.includes(val)) {
+			setExactValue(val);
+		} else if (!val) {
+			const currentValueIndex = arr.indexOf(value);
+
+			if (currentValueIndex !== arr.length - 1) {
+				setExactValue(arr[currentValueIndex + 1]);
+			} else {
+				setExactValue(arr[0]);
+			}
+		}
+	};
+
+	return [value, toggle];
+};
