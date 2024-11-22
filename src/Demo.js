e556cf5e34ba8eecfb30ec4e1a3f164d26bc5f0f
Konstantin1spb <105925642+sitepreview@users.noreply.github.com>
2024-11-22 13:48:30 +0300

    initial commit

---
+++ src/Demo.js
@@ -0,0 +1,9 @@
+import { useToggle } from './hooks/useToggle';
+
+function Demo() {
+	const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
+
+	return <button onClick={() => toggle()}>{value}</button>;
+}
+
+export default Demo;
